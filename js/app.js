b8595b1eb057cd9b3004062cf498062f627eae6f
Matthew <Mattxsc@gmail.com>
2018-06-09 20:53:18 -0400

    Updated angular app
    Updated formatting, changed route paths

--- js/app.js
+++ js/app.js
@@ -1,34 +1,29 @@
-var app = angular.module('noveriaApp', ['ngRoute']);
+var app = angular.module('noveriaApp', ['ngRoute'])
 	
-app.config(function($routeProvider) {
-	$routeProvider.when('Noveria/',
-	{
+.config(function ($routeProvider) {
+	$routeProvider.when('/', {
 		controller: 'HomeController',
 		templateUrl: 'home.html'
-	});
-	$routeProvider.when('Noveria/#!/#%2Ffacts',
+	})
+	.when('#/facts',
 	{
 		controller: 'FactsController',
 		templateUrl: 'facts.html'
-	});
-	$routeProvider.when('Noveria/#!/#%2Fabout',
-	{
+	})
+	.when('/#/about', {
 		controller: 'AboutController',
 		templateUrl: 'about.html'
-	});
-	$routeProvider.when('Noveria/#!/#%2Fquiz',
-	{
+	})
+	.when('/#quiz', {
 		controller: 'QuizController',
 		controllerAs: 'quiz',
 		templateUrl: 'quiz.html'
-	});
-	$routeProvider.when('Noveria/#!/#%2Fjoin',
-	{
+	})
+	.when('/join', {
 		controller: 'JoinController',
 		templateUrl: 'join.html'
-	});
-	$routeProvider.otherwise(
-	{ 
+	})
+	.otherwise({ 
 		redirectTo: '/',
 		//controller: 'WhoopsController',
 		//templateUrl: '404.html' 
@@ -41,35 +36,31 @@ app.controller('HomeController', function() {
 	self.currentYear = (new Date()).getFullYear();
 
 	self.navClicked = function(button) {
-		if(button === "home") {
+		if (button === "home") {
 			$("#homeBtn").addClass("clicked");
 			$("#factsBtn").removeClass("clicked");
 			$("#aboutBtn").removeClass("clicked");
 			$("#quizBtn").removeClass("clicked");
 			$("#joinBtn").removeClass("clicked");
-		}
-		else if(button === "facts") {
+		} else if (button === "facts") {
 			$("#homeBtn").removeClass("clicked");
 			$("#factsBtn").addClass("clicked");
 			$("#aboutBtn").removeClass("clicked");
 			$("#quizBtn").removeClass("clicked");
 			$("#joinBtn").removeClass("clicked");
-		}
-		else if(button === "about") {
+		} else if (button === "about") {
 			$("#homeBtn").removeClass("clicked");
 			$("#factsBtn").removeClass("clicked");
 			$("#aboutBtn").addClass("clicked");
 			$("#quizBtn").removeClass("clicked");
 			$("#joinBtn").removeClass("clicked");
-		}
-		else if(button === "quiz") {
+		} else if (button === "quiz") {
 			$("#homeBtn").removeClass("clicked");
 			$("#factsBtn").removeClass("clicked");
 			$("#aboutBtn").removeClass("clicked");
 			$("#quizBtn").addClass("clicked");
 			$("#joinBtn").removeClass("clicked");
-		}
-		else {
+		} else {
 			$("#homeBtn").removeClass("clicked");
 			$("#factsBtn").removeClass("clicked");
 			$("#aboutBtn").removeClass("clicked");
@@ -86,7 +77,8 @@ app.controller('AboutController', function() {});
 app.controller('QuizController', function($scope) {
 	var self = $scope;
 
-	self.questions = [{ 'question': "Abortion should be legal in cases of rape, incest, socioeconomic condition, or risk of maternal health.", 'direction': -1, 'axis': 's', 'choice': 0 },
+	self.questions = [
+		{ 'question': "Abortion should be legal in cases of rape, incest, socioeconomic condition, or risk of maternal health.", 'direction': -1, 'axis': 's', 'choice': 0 },
 		{ 'question': "Women who have illegal abortions should be punished.", 'direction': 1, 'axis': 's', 'choice': 0 },
 		{ 'question': "Recreational drugs such as cannabis should be legalized or decriminalized.", 'direction': -1, 'axis': 's', 'choice': 0 },
 		{ 'question': "Drug dealers/traffickers should receive mandatory minimum prison sentences.", 'direction': 1, 'axis': 's', 'choice': 0 },
@@ -121,7 +113,9 @@ app.controller('QuizController', function($scope) {
 		{ 'question': "Privatizing the school system will result in greater choice and higher quality education.", 'direction': 1, 'axis': 'e', 'choice': 0 },
 		{ 'question': "The government should provide a college education to every citizen.", 'direction': -1, 'axis': 'e', 'choice': 0 },
 		{ 'question': "The government should bail out businesses to prevent them from failing.", 'direction': -1, 'axis': 'e', 'choice': 0 },
-		{ 'question': "Regulations on businesses generally do more harm than good.", 'direction': 1, 'axis': 'e', 'choice': 0 }];
+		{ 'question': "Regulations on businesses generally do more harm than good.", 'direction': 1, 'axis': 'e', 'choice': 0 }
+	];
+	
 	self.index = 0;
 	self.score = [0, 0];
 	
@@ -133,8 +127,7 @@ app.controller('QuizController', function($scope) {
 			
 			$(prevChoiceBtn).removeClass('active');
 			$('#choice' + self.questions[self.index].choice).addClass('active');
-		}
-		else if (self.index === self.questions.length - 1) {
+		} else if (self.index === self.questions.length - 1) {
 			self.index++;
 		}
 	};
@@ -147,8 +140,7 @@ app.controller('QuizController', function($scope) {
 				self.index--;
 				$(prevChoiceBtn).removeClass('active');
 				$('#choice' + self.questions[self.index].choice).addClass('active');
-			}
-			else {
+			} else {
 				self.index--;
 			}
 		}
@@ -174,13 +166,17 @@ app.controller('QuizController', function($scope) {
 
 	var drawChart = function() {
 		$('canvas').show();
+		
 		var canvas = document.getElementById('canvas');
 		var ctx = canvas.getContext('2d');
+		
 		canvas.width = 500;
 		canvas.height = 500;
 		ctx.fillStyle = 'rgb(154, 154, 154)';
+		
 		for (var x = 25; x < 500; x += 25) { ctx.fillRect(x, 0, 1, 500); }
 		for (var y = 25; y < 500; y += 25) { ctx.fillRect(0, y, 500, 1); }
+		
 		ctx.fillStyle = 'rgb(50, 50, 50)';
 		ctx.fillRect(0, 249, 500, 3);
 		ctx.fillRect(249, 0, 3, 500);
@@ -198,13 +194,13 @@ app.controller('QuizController', function($scope) {
 
 app.controller('JoinController', function($scope) {
 	$scope.message = "join";
-	$scope.sendMail = function() {
-    	var link = "mailto:noverianfederation@gmail.com"
-            + "&subject=" + escape("New Citizen Request") 
-            + "&body=" + escape($scope.user.name + " (" + $scope.user.email + ") from " 
-            + $scope.user.location + " wishes to become a citizen of Noveria. " + $scope.user.name + " also belongs to " + $scope.user.micros + ".");
-        console.log("test test");
-    	window.location.href = link;
+	$scope.sendMail = function () {
+		var link = "mailto:noverianfederation@gmail.com" + 
+		    	   "&subject=" + escape("New Citizen Request") + 
+		    	   "&body=" + escape($scope.user.name + " (" + $scope.user.email + ") from " + $scope.user.location + 
+			   " wishes to become a citizen of Noveria. " + $scope.user.name + " also belongs to " + $scope.user.micros + ".";
+		console.log("test test");
+    		window.location.href = link;
 	};
 });
 
